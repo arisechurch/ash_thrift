5a2748b9cbdb35cdcfa4c6b09bd5a4a5313d9255
Tim Smart <tim.smart@arisechurch.com>
2021-11-15 10:06:35 +1300

    Add description

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule AshThrift.MixProject do
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
+      description: description(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_paths: ["lib"],
@@ -31,6 +32,12 @@ defmodule AshThrift.MixProject do
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/arisechurch/ash_thrift"}
     ]
+
+  defp description do
+    """
+    Thrift extension for the Ash framework.
+    """
+  end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
